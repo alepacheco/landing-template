3943f1726fe7e6d1c40e5acf50780adcf1e0ad1d
Alejandro Pacheco <alepacheco99@gmail.com>
2019-12-16 13:17:12 +0100

    Add styled components SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
 import '../css/index.scss';
 import HelloWorld from '../components/HelloWorld';
+import styled from 'styled-components';
 
+const Page = styled.div`
+  margin-top: 10vh;
+  font-family: monospace;
+  text-align: center;
+`;
 const Index = () => {
   return (
-    <div className="page">
+    <Page>
       <HelloWorld />
-
-      <style jsx>{`
-        .page {
-          margin-top: 10vh;
-          font-family: monospace;
-          text-align: center;
-        }
-      `}</style>
-    </div>
+    </Page>
   );
 };
 
